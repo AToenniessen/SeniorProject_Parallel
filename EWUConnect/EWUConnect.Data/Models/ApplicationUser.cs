9fc3ab0feefb8906009380e81536933eda48ba0b
unknown <atoenniessen@eagles.ewu.edu>
2019-05-18 14:48:35 -0700

    fixed reference error to 'HEAD', started Service project

---
+++ EWUConnect/EWUConnect.Data/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace EWUConnect.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ EWUConnect/EWUConnect.Service/ForumService.cs
@@ -0,0 +1,54 @@
+﻿using EWUConnect.Data;
+using EWUConnect.Data.Models;
+using EWUConnectMVC.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EWUConnect.Service
+{
+	public class ForumService : IForum
+	{
+		private readonly ApplicationDbContext dbContext;
+		public ForumService(ApplicationDbContext context)
+		{
+			dbContext = context;
+		}
+
+		public Task Create(Forum forum)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task Delete(int forumId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Forum> GetAll()
+		{
+			return dbContext.Forums.Include(forum => forum.Posts);
+		}
+
+		public IEnumerable<ApplicationUser> GetAllActiveUsers()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Forum GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateForumDescription(int forumId, string newDescription)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateForumTitle(int forumId, string newTitle)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
